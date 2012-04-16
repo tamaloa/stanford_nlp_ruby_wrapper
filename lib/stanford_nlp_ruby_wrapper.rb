059e0b58ea8a60577c27b8600798ff016543b910
Michael Prilop <prilop@informatik.uni-leipzig.de>
2012-04-16 14:13:27 +0200

    updated stanford-nlp to newest versions

--- lib/stanford_nlp_ruby_wrapper.rb
+++ lib/stanford_nlp_ruby_wrapper.rb
@@ -6,20 +6,18 @@ module StanfordNlpRubyWrapper
 
   raise(Exception, "You have to set a JAVA_HOME environment variable for rjb to work!") unless ENV['JAVA_HOME']
 
-  Rjb::load('bin/stanford-postagger.jar:bin/stanford-ner.jar', ['-Xmx200m'])
+  Rjb::load('bin/stanford-postagger-2012-03-09/stanford-postagger.jar:bin/stanford-ner-2012-04-07/stanford-ner.jar', ['-mx400m'])
 
   CRFClassifier = Rjb::import('edu.stanford.nlp.ie.crf.CRFClassifier')
-  classifier = CRFClassifier.getClassifierNoExceptions("bin/ner-eng-ie.crf-4-conll.ser.gz")
+  classifier = CRFClassifier.getClassifierNoExceptions("bin/stanford-ner-2012-04-07/classifiers/english.all.3class.distsim.crf.ser.gz")
 
   MaxentTagger = Rjb::import('edu.stanford.nlp.tagger.maxent.MaxentTagger')
-  MaxentTagger.init("bin/left3words-wsj-0-18.tagger")
+  tagger = MaxentTagger.new("bin/stanford-postagger-2012-03-09/models/wsj-0-18-left3words.tagger")
 
-  Sentence = Rjb::import('edu.stanford.nlp.ling.Sentence')
-
-  sent = "Good afternoon Rajat Raina, how are you today?"
+  sentence = "Good afternoon Rajat Raina, how are you today?"
 
   Measure.time "NER", 10 do
-    puts classifier.testString( sent )
-    puts MaxentTagger.tagString( sent )
+    puts classifier.classifyToString sentence
+    puts tagger.tagString sentence
   end
 end
